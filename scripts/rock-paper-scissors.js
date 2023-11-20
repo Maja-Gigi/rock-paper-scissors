a17acf510a737c29ce5607fb01a44f65363d82a6
Maja-Gigi <maja.hreljak@gmail.com>
2023-11-20 14:06:11 +0100

    Version4

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,3 +1,13 @@
+// Update the score
+
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+
+updateScoreElement();
+
 // Definiramo kako kompjuter radi korake
 function pickComputerMove() {
   const randomNumber = Math.random();
@@ -51,7 +61,24 @@ function playGame(playerMove) {
     }
   }
 
+  if (result === 'You win.') {
+    score.wins++;
+  } else if (result === 'You lose.') {
+    score.losses++;
+  } else if (result === 'Tie.') {
+    score.ties++;
+  }
 
+  localStorage.setItem('score', JSON.stringify(score));
 
-  alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}`);
+  document.querySelector('.js-moves').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}`;
+  
+  document.querySelector('.js-result').innerHTML = `${result}`;
+
+  updateScoreElement();
+   
+}
+
+function updateScoreElement() {
+  document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
